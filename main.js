09bdbbc774e51e393b2a1cb09b2e90767a469b23
lozinha07 <124881217+lozinha07@users.noreply.github.com>
2023-11-25 17:24:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+
+//ariana
+function tocaSomAriana(){
+    document.querySelector("#som_tecla_ariana").play();
+}
+document.querySelector(".tecla_ariana").onclick = tocaSomAriana;
+
+//ashnikko
+function tocaSomAshnikko(){
+    document.querySelector("#som_tecla_ashnikko").play();
+}
+document.querySelector(".tecla_ashnikko").onclick = tocaSomAshnikko;
+
+//eminem
+function tocaSomEminem(){
+    document.querySelector("#som_tecla_eminem").play();
+}
+document.querySelector(".tecla_eminem").onclick = tocaSomEminem;
+
+//hariel
+function tocaSomHariel(){
+    document.querySelector("#som_tecla_hariel").play();
+}
+document.querySelector(".tecla_hariel").onclick = tocaSomHariel;
+
+//jorgeemateus
+function tocaSomJorgeemateus(){
+    document.querySelector("#som_tecla_jorgeemateus").play();
+}
+document.querySelector(".tecla_jorgeemateus").onclick = tocaSomJorgeemateus;
+
+//kevin
+function tocaSomKevin(){
+    document.querySelector("#som_tecla_kevin").play();
+}
+document.querySelector(".tecla_kevin").onclick = tocaSomKevin;
+
+//lilpeep
+function tocaSomLilpeep(){
+    document.querySelector("#som_tecla_lilpeep").play();
+}
+document.querySelector(".tecla_lilpeep").onclick = tocaSomLilpeep;
+
+//livinho
+function tocaSomLivinho(){
+    document.querySelector("#som_tecla_livinho").play();
+}
+document.querySelector(".tecla_livinho").onclick = tocaSomLivinho;
+
+//marilia
+function tocaSomMarilia(){
+    document.querySelector("#som_tecla_marilia").play();
+}
+document.querySelector(".tecla_marilia").onclick = tocaSomMarilia;
+
+/*
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+let lista = document.querySelectorAll(".tecla");
+let contador = 0;
+while(contador< lista.length){
+    const efeito = lista[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    lista(contador).onclick = function(){
+        tocaSom(idAudio);
+    }
+    contador++;
+}*/
